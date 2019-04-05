70dfae168d3a54906fefb458366884a8b1697ff0
narapon <43005911+narapons@users.noreply.github.com>
2019-04-06 01:33:05 +0900

    Add files via upload

--- src/EcpnomyBank/main.php
+++ src/EcpnomyBank/main.php
@@ -1,1 +1,34 @@
+<?php
 
+    namespace EconomyBank;
+
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\utils\Config;
+
+    class main extends PluginBase
+    {
+        public $money;
+        public $formId;
+        const SUCCESS_TAG = "§a【運営】 §f";
+        const ERROR_TAG = "§a【運営】 §c";
+
+        public function onEnable(): void
+        {
+            $this->getServer()->getPluginManager()->registerEvents(new eventListener($this), $this);
+            $this->getServer()->getCommandMap()->register("bank", new bankCommand($this));
+            if (!file_exists($this->getDataFolder())) {
+                mkdir($this->getDataFolder(), 0777);
+            }
+            $this->money = new Config($this->getDataFolder() . "money.yml", Config::YAML);
+            $this->formId[0] = mt_rand(50000, 100000);
+            $this->formId[1] = mt_rand(50000, 100000);
+            $this->formId[2] = mt_rand(50000, 100000);
+            $this->formId[3] = mt_rand(50000, 100000);
+            $this->getScheduler()->scheduleRepeatingTask(new saveTask($this), 20 * 60 * 1);//5分ごと
+        }
+
+        public function onDisable(): void
+        {
+            $this->money->save();
+        }
+    }
